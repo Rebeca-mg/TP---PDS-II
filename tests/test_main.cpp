b4c6633ee75cddc404f000ffb7748010079a2ae0
Kristyn Hoffmann <hoffmannkristyn@gmail.com>
2025-06-25 21:17:03 -0300

    Add files via upload

---
+++ tests/test_main.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file test_main.cpp
+ * @brief Arquivo principal dos testes unitários usando doctest
+ * @author Projeto Acadêmico C++11
+ * @date 2025
+ * @version 1.0
+ */
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
